d8122fd3c0798ee36dd4d27b14d5293b62f02e74
Harshdeep Dhatt <hdhatt@codeaurora.org>
2019-04-19 11:22:51 -0600

    msm: kgsl: Fix drawqueue timer race condition
    Update the drawqueue timer while holding the device mutex
    to make sure we don't trigger the timeout incorrectly.
    
    Change-Id: I19fbe7847b7e69c12ea903b4d3ee18b9438ccb6d
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -653,14 +653,6 @@ static int sendcmd(struct adreno_device *adreno_dev,
 //		time.ticks, (unsigned long) secs, nsecs / 1000, drawctxt->rb,
 //		adreno_get_rptr(drawctxt->rb));
 
-	mutex_unlock(&device->mutex);
-
-	cmdobj->submit_ticks = time.ticks;
-
-	dispatch_q->cmd_q[dispatch_q->tail] = cmdobj;
-	dispatch_q->tail = (dispatch_q->tail + 1) %
-		ADRENO_DISPATCH_DRAWQUEUE_SIZE;
-
 	/*
 	 * For the first submission in any given command queue update the
 	 * expected expire time - this won't actually be used / updated until
@@ -671,6 +663,14 @@ static int sendcmd(struct adreno_device *adreno_dev,
 	if (dispatch_q->inflight == 1)
 		dispatch_q->expires = jiffies +
 			msecs_to_jiffies(adreno_drawobj_timeout);
+
+	mutex_unlock(&device->mutex);
+
+	cmdobj->submit_ticks = time.ticks;
+
+	dispatch_q->cmd_q[dispatch_q->tail] = cmdobj;
+	dispatch_q->tail = (dispatch_q->tail + 1) %
+		ADRENO_DISPATCH_DRAWQUEUE_SIZE;
 
 	/*
 	 * If we believe ourselves to be current and preemption isn't a thing,
