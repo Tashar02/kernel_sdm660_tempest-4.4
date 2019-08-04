e8863a1ff16ef559fead3aedac6658375045851a
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-04 10:48:35 +0530

    power: smb-lib: Silence spamming debugging
    Absolutely destroys dmesg on a plugged-in device
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -40,6 +40,7 @@ extern int hwc_check_global;
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
 
+#ifdef CONFIG_DEBUG_SMB_LIB
 #define smblib_dbg(chg, reason, fmt, ...)			\
 	do {							\
 		if (*chg->debug_mask & (reason))		\
@@ -50,7 +51,6 @@ extern int hwc_check_global;
 				__func__, ##__VA_ARGS__);	\
 	} while (0)
 #else
-#define smblib_err(chg, fmt, ...) do {} while (0)
 #define smblib_dbg(chg, reason, fmt, ...) do {} while (0)
 #endif
 
@@ -4086,10 +4086,12 @@ int smblib_get_prop_slave_current_now(struct smb_charger *chg,
 #ifdef DEBUG
 irqreturn_t smblib_handle_debug(int irq, void *data)
 {
+#ifdef CONFIG_DEBUG_SMB_LIB
 	struct smb_irq_data *irq_data = data;
 	struct smb_charger *chg = irq_data->parent_data;
 
 	smblib_dbg(chg, PR_INTERRUPT, "IRQ: %s\n", irq_data->name);
+#endif
 	return IRQ_HANDLED;
 }
 #else
