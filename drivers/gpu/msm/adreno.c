bea3b42f5c1e9bba87c28efec5e741bc794324f0
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-03-15 11:02:32 +0700

    msm: adreno: Disable snapshot, and coresight when debug kernel is disabled
    Co-authored-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -625,7 +625,9 @@ static irqreturn_t adreno_irq_handler(struct kgsl_device *device)
 		tmp &= ~BIT(i);
 	}
 
+#ifdef CONFIG_DEBUG_KERNEL
 	gpudev->irq_trace(adreno_dev, status);
+#endif
 
 	/*
 	 * Clear ADRENO_INT_RBBM_AHB_ERROR bit after this interrupt has been
@@ -1020,6 +1022,7 @@ static int adreno_probe(struct platform_device *pdev)
 
 	kgsl_pwrscale_init(&pdev->dev, CONFIG_QCOM_ADRENO_DEFAULT_GOVERNOR);
 
+#ifdef CONFIG_DEBUG_KERNEL
 	/* Initialize coresight for the target */
 	adreno_coresight_init(adreno_dev);
 
@@ -1039,6 +1042,7 @@ static int adreno_probe(struct platform_device *pdev)
 #endif
 
 	place_marker("M - DRIVER GPU Ready");
+#endif
 
 out:
 	if (status) {
@@ -1095,7 +1099,9 @@ static int adreno_remove(struct platform_device *pdev)
 #endif
 	adreno_sysfs_close(adreno_dev);
 
+#ifdef CONFIG_DEBUG_KERNEL
 	adreno_coresight_remove(adreno_dev);
+#endif
 	adreno_profile_close(adreno_dev);
 
 	kgsl_pwrscale_close(device);
@@ -1425,8 +1431,10 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	/* Start the GPU */
 	gpudev->start(adreno_dev);
 
+#ifdef CONFIG_DEBUG_KERNEL
 	/* Re-initialize the coresight registers if applicable */
 	adreno_coresight_start(adreno_dev);
+#endif
 
 	adreno_irqctrl(adreno_dev, 1);
 
@@ -1559,8 +1567,10 @@ static int adreno_stop(struct kgsl_device *device)
 
 	adreno_ocmem_free(adreno_dev);
 
+#ifdef CONFIG_DEBUG_KERNEL
 	/* Save active coresight registers if applicable */
 	adreno_coresight_stop(adreno_dev);
+#endif
 
 	/* Save physical performance counter values before GPU power down*/
 	adreno_perfcounter_save(adreno_dev);
@@ -2201,8 +2211,10 @@ static int adreno_soft_reset(struct kgsl_device *device)
 	/* Reinitialize the GPU */
 	gpudev->start(adreno_dev);
 
+#ifdef CONFIG_DEBUG_KERNEL
 	/* Re-initialize the coresight registers if applicable */
 	adreno_coresight_start(adreno_dev);
+#endif
 
 	/* Enable IRQ */
 	adreno_irqctrl(adreno_dev, 1);
