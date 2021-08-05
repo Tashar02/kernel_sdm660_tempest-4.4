c60496be06924273110d970c55a73ed16b435a7a
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-08-05 15:56:51 +0600

    q6core: Export symbol service_version and fwk_version_size correctly
    Fixes the following logs:
    
    [42.865058] q6core_send_get_avcs_fwk_ver_cmd: DSP returned error[ADSP_EUNSUPPORTED]
    [42.855072] q6core_get_service_version: Failed to get service size for service id 7 with error -95
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- sound/soc/msm/qdsp6v2/q6core.c
+++ sound/soc/msm/qdsp6v2/q6core.c
@@ -375,7 +375,7 @@ struct cal_block_data *cal_utils_get_cal_block_by_key(
 	return NULL;
 }
 
-#ifndef CONFIG_MACH_XIAOMI_MSM8998
+#if 0
 static int q6core_send_get_avcs_fwk_ver_cmd(void)
 {
 	struct apr_hdr avcs_ver_cmd;
@@ -477,7 +477,6 @@ int q6core_get_service_version(uint32_t service_id,
 	pr_err("%s: No service matching service ID %d\n", __func__, service_id);
 	return -EINVAL;
 }
-EXPORT_SYMBOL(q6core_get_service_version);
 
 size_t q6core_get_fwk_version_size(uint32_t service_id)
 {
@@ -534,15 +533,22 @@ size_t q6core_get_fwk_version_size(uint32_t service_id)
 done:
 	return ret;
 }
-EXPORT_SYMBOL(q6core_get_fwk_version_size);
-
 #else
 int q6core_get_service_version(uint32_t service_id,
 			       struct avcs_fwk_ver_info *ver_info,
-			       size_t size) { return -1; }
+			       size_t size)
+{
+	return -1;
+}
+
 size_t q6core_get_fwk_version_size(uint32_t service_id)
-	{ return -1; }
+{
+	return -1;
+}
 #endif
+EXPORT_SYMBOL(q6core_get_service_version);
+EXPORT_SYMBOL(q6core_get_fwk_version_size);
+
 int32_t core_set_license(uint32_t key, uint32_t module_id)
 {
 	struct avcs_cmd_set_license *cmd_setl = NULL;
