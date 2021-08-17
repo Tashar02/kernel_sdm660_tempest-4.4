bfa3d6bb841e9a49490b06dd22bd599ae388d110
Todd Kjos <tkjos@google.com>
2021-08-17 14:23:28 -0700

    ANDROID: xt_quota2: set usersize in xt_match registration object
    Explicitly set what is visible to userspace
    
    Bug: 196046570
    Test: passed netd test suites
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: Iacec0ef8ae290e01f1b60508d8abcd40a3653c83
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- net/netfilter/xt_quota2.c
+++ net/netfilter/xt_quota2.c
@@ -323,6 +323,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 	{
@@ -333,6 +334,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 };
